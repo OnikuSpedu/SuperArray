8ff270c88166d603dc58e6682e4d9865b08d6d46
OnikuSpedu <srakibnyc1@gmail.com>
2020-11-02 15:31:02 -0500

    Added size method and add method.

--- SuperArray.java
+++ SuperArray.java
@@ -14,4 +14,19 @@ public class SuperArray {
     public int size() {
         return this.size;
     }
+
+    public boolean add(String element) {
+        if (this.data.length > this.size) {
+
+            this.data[size] = element;
+            this.size += 1;
+
+            if (this.data[size].equals(element)) { //Verifies if it was changed
+                return true;
+            }
+        }
+
+        return false; //If their is not enough capacity or the adding fails it returns false
+        
+    }
 }
