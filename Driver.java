138fe349fc444f2f07ff497083ba7529bc693707
OnikuSpedu <srakibnyc1@gmail.com>
2020-11-04 13:01:44 -0500

    Nicer looking tests

--- Driver.java
+++ Driver.java
@@ -1,20 +1,12 @@
 public class Driver {
     public static void main(String[] args) {
         SuperArray words = new SuperArray();
-        words.add("kani");
-        words.add("uni");
-        words.add("ebi");
-        words.add("kani");
-        words.add("uni");
-        words.add("ebi");
-        words.add("kani");
-        words.add("uni");
-        words.add("ebi");
-        words.add("kani");
-        words.add("uni");
-        words.add("ebi");
+        String[] addWords = {"kani", "uni", "ebi", "Hey", "this", "is", "a", "test", "yay", "apples", "oranges", "bananas"};
+        for(int i = 0; i < addWords.length; i++){
+            System.out.println("Added \"" + addWords[i] + "\": " + words.add(addWords[i]));
+        }
         for(int i = 0; i < words.size(); i++){
-            System.out.println( words.get(i) );
+            System.out.println("At index " + i + ": " + words.get(i));
         }
         System.out.println("Array Data: " + words.toString());
         System.out.println("Empty?: " + words.isEmpty());
